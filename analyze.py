0cd2c9a10d1ed2883c6bb9876fc362c4c4b61c67
becca <beccabthomson@gmail.com>
2019-11-26 13:00:05 -0800

    some metrics, omitted Electronic, R&B, Indie

--- analyze.py
+++ analyze.py
@@ -1,32 +1,41 @@
 import sys
 
+import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.linear_model import SGDClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 
 
 def predict_genre(df):
-    x = df['lyrics'].astype('U')
-    y = df['genre'].astype('U')
+    # x = df['lyrics'].astype('U')
+    # y = df['genre'].astype('U')
+
+    # Test removing genres with lowest accuracy
+    x = df.loc[~df.genre.isin(['Electronic', 'R&B', 'Indie'])]['lyrics'].astype('U')
+    y = df.loc[~df.genre.isin(['Electronic', 'R&B', 'Indie'])]['genre'].astype('U')
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.10, random_state=99)
 
-    print("sgd classifier:") # 59%
+    print("sgd classifier:")
     sgd_clf = Pipeline([
         ('vect', CountVectorizer()),
-        # we get worse accuracy using balanced class weight
-        # ('clf', SGDClassifier(loss='log', penalty='l2', alpha=1e-3, random_state=42, max_iter=30, class_weight='balanced'))
-        ('clf', SGDClassifier(loss='log', penalty='l2', alpha=1e-3, random_state=42, max_iter=30))
+        ('clf',
+         SGDClassifier(loss='log', penalty='l2', alpha=1e-3, random_state=42, max_iter=30, class_weight='balanced'))
     ])
     sgd_clf.fit(x_train, y_train)
     sgd_predicted = sgd_clf.predict(x_test)
     print(accuracy_score(y_test, sgd_predicted))
+    sgd_cm = confusion_matrix(y_test, sgd_predicted)
+    print(sgd_cm)  # Matrix
+    sgd_cm = sgd_cm.astype('float') / sgd_cm.sum(axis=1)[:, np.newaxis]
+    print(sgd_cm.diagonal())  # Accuracy scores
+    print(classification_report(y_test, sgd_predicted)) #Metrics
 
-    print("mnb classifier:") # 45%
+    print("mnb classifier:")  # 45%
     mnb_clf = Pipeline([
         ('vect', CountVectorizer()),
         ('clf', MultinomialNB(alpha=0.1, fit_prior=True, class_prior=None)),
